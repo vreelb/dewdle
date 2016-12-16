507bd0cd1125ee8a9789287d0079f6b8488b5411
Ben Vreeland <vreelb@gmail.com>
2016-12-16 09:48:41 -0500

    Abstracts out WebSocket code to separate file.
    I should have done this from the beginning, really.

---
+++ js/sockets.js
@@ -0,0 +1,38 @@
+function openSocket(URL) {
+	if ("WebSocket" in window) {
+		var open = false;
+		var ws = new WebSocket(URL);
+
+		ws.onopen = function() {
+			console.log('connection opened');
+			open = true;
+		};
+
+		ws.onmessage = function (evt) {
+			evalMessage(evt.data);
+		};
+
+		ws.onclose = function() {
+			if (open) {
+				console.log('connection closed');
+				setTimeout( function() {
+					console.log('trying to reconnect...');
+					open = false;
+					openSocket(URL);
+				}, 1000); // try to reconnect every second
+			}
+		};
+
+		ws.onerror = function() {
+			console.log('connection error');
+			setTimeout( function() {
+				console.log('trying to connect again...');
+				openSocket(URL);
+			}, 1000); // try to reconnect every second
+		};
+
+	} else {
+		console.log('this browser does not support web sockets...');
+	}
+	return ws;
+}
