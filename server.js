588ac2ed5aeba06724283a1ffe21cbfc23b3afc2
Ben Vreeland <vreelb@gmail.com>
2017-02-05 20:46:42 -0500

    Adds support for multiple instances of any page.
    In practice it probably wouldn't be a good idea to try live...
    but at least there wouldn't be any issues overwriting sockets now.

--- server.js
+++ server.js
@@ -4,15 +4,42 @@ var http = require("http");
 var net = require("net");
 var sendHeartbeats = require('ws-heartbeats');
 
-var wss = new (require("ws")).Server({port: PORT}), sockets = {}
+var wss = new (require("ws")).Server({port: PORT});
+var sockets_draw = [];
+var sockets_control = [];
+var sockets_render = [];
 
 var temp_canvas = '{"objects":[],"background":""}';
 var status = "DOWN";
 
+function socketSend(page, message) {
+	if (page == "draw") {
+		sockets_draw.forEach(function(sock) {
+			sock.send(message);
+		});
+	} else if (page == "control") {
+		sockets_control.forEach(function(sock) {
+			sock.send(message);
+		});
+	} else if (page == "render") {
+		sockets_render.forEach(function(sock) {
+			sock.send(message);
+		});
+	}
+}
+
 wss.on("connection", function connection(ws) {
 	var page = ws.upgradeReq.url.substring(1);		// see who connected
-	sockets[page] = ws;								// save that for later...
-	console.log(page+" connected");
+	var place = -1;
+	if (page == "draw") {							// save that for later...
+		place = sockets_draw.push(ws);
+	} else if (page == "control") {
+		place = sockets_control.push(ws);
+	} else if (page == "render") {
+		place = sockets_render.push(ws);
+	}
+	place--;
+	console.log(page+" "+place+" connected");
 
 	sendHeartbeats(ws, {"heartbeatTimeout":30000, "heartbeatInterval":10000});
 
@@ -22,35 +49,33 @@ wss.on("connection", function connection(ws) {
 	}
 
 	ws.on("message", function incoming(message) {
-		console.log("received '"+message+"' from "+page);
+		console.log("received '"+message+"' from "+page+" "+place);
 
-		if (sockets["draw"] && sockets["control"] && sockets["render"]) {
-			switch (message) {
-				case "UP":			// going on air
-				case "DOWN":		// going off air
-					status = message;		// keep track of the status either way
-					sockets["draw"].send(message);
-					sockets["control"].send(message);
-					break;
-				default:			// sending a canvas
-					temp_canvas = message; // keep track of the canvas state
-					if (page != "draw") {
-						sockets["draw"].send(message);
-					}
-					if (page != "control") {
-						sockets["control"].send(message);
-					}
-					sockets["render"].send(message);
-			}
-		} else {
-			console.log("not all pages connected, correct before proceeding\npages connected: "+Object.keys(sockets));
+		switch (message) {
+			case "UP":					// going on air
+			case "DOWN":				// going off air
+				status = message;		// keep track of the status either way
+				socketSend("draw", message);
+				socketSend("control", message);
+				break;
+			default:					// sending a canvas
+				temp_canvas = message;	// keep track of the canvas state
+				socketSend("draw", message);
+				socketSend("control", message);
+				socketSend("render", message);
 		}
 
 	});
 
 	ws.on("close", function () {
-		delete sockets[page]
-		console.log(page+" disconnected")
+		if (page == "draw") {
+			delete sockets_draw[place];
+		} else if (page == "control") {
+			delete sockets_control[place];
+		} else if (page == "render") {
+			delete sockets_render[place];
+		}
+		console.log(page+" "+place+" disconnected");
 	});
 });
 
